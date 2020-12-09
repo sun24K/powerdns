2147c6499a3dd976d94acf3e112405cf80f0ad0b
root <root@localhost.localdomain>
2020-12-09 05:40:30 -0500

    first commit

---
+++ config.inc.php
@@ -0,0 +1,14 @@
+<?php
+$db_host = 'localhost';
+$db_name = 'powerdns';
+$db_user = 'admin';
+$db_pass = 'sun';
+$db_type = 'mysql';
+
+$session_key = '5gQ8c7FcYQ!VxPfoiJnL)mAYpAJaZID4Vc#HRU%mp24-9v';
+
+$iface_lang = 'en_EN';
+
+$dns_hostmaster = '';
+$dns_ns1 = '';
+$dns_ns2 = '';
